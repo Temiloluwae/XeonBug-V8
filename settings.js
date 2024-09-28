1f2d0dd7fa3fad8013a43391b1d47c89391c3506
Temiloluwae <161668702+Temiloluwae@users.noreply.github.com>
2024-09-28 12:27:24 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,15 +10,15 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
-global.ytname = "YT: GlobalTechInfo"
-global.socialm = "GitHub: GlobalTechInfo"
+global.ownernomer = "2349116186010"
+global.ownername = "SMILE
+global.ytname = "YT: Anonymous"
+global.socialm = "GitHub: Emma12u3"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
-global.botname = 'XeonBug-V8' //name of the bot
+global.ownernumber = '2349116186010'  //creator number
+global.ownername = 'SMILE' //owner name
+global.botname = 'SMILE-V8' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
@@ -41,7 +41,7 @@ global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = false //auto update bio
 global.anti91 = false //auto block +91 
-global.autoswview = false //auto view status/story
+global.autoswview = true //auto view status/story
 
 //menu type 
 //v1 is image menu, 
